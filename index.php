04e33af505fe5265528277b0a8a01341dca80dd0
Kadzharduzov <romankadzharduzov@yandex.ru>
2026-05-19 10:09:05 +0300

    Update

--- index.php
+++ index.php
@@ -28,19 +28,7 @@
             </form>
         </div>
         
-        <div class="examples">
-            <h3>📋 Примеры для проверки:</h3>
-            <div class="example-buttons">
-                <a href="solve.php?eq=X+3=7" class="example-btn">X + 3 = 7</a>
-                <a href="solve.php?eq=X-5=10" class="example-btn">X - 5 = 10</a>
-                <a href="solve.php?eq=2X=8" class="example-btn">2X = 8</a>
-                <a href="solve.php?eq=3X+4=19" class="example-btn">3X + 4 = 19</a>
-                <a href="solve.php?eq=5X-7=23" class="example-btn">5X - 7 = 23</a>
-                <a href="solve.php?eq=X/2=6" class="example-btn">X / 2 = 6</a>
-                <a href="solve.php?eq=4X=20" class="example-btn">4X = 20</a>
-                <a href="solve.php?eq=X+12=25" class="example-btn">X + 12 = 25</a>
-            </div>
-        </div>
+        
     </div>
 </body>
 </html>
